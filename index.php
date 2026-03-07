c2bb0e41630d59d34582f44036c8410ebbd38f8a
blarkin13 <larki.brian@gmail.com>
2026-03-07 08:34:32 -0600

    Update font paths to use scout_owl directory

--- index.php
+++ index.php
@@ -8,22 +8,22 @@
     <style>
       @font-face {
       font-family: 'Roboto';
-      src: url('/../stat_owl/fonts/roboto/Roboto-Regular.ttf') format('ttf'),
-      url('/../stat_owl/fonts/roboto/Roboto-Regular.ttf') format('ttf');
+      src: url('/../scout_owl/fonts/roboto/Roboto-Regular.ttf') format('ttf'),
+      url('/../scout_owl/fonts/roboto/Roboto-Regular.ttf') format('ttf');
       font-weight: normal;
       font-style: normal;
       }
       @font-face {
       font-family: 'Griffy';
-      src: url('/../stat_owl/fonts/Griffy/Griffy-Regular.ttf') format('ttf'),
-      url('/../stat_owl/fonts/Griffy/Griffy-Regular.ttf') format('ttf');
+      src: url('/../scout_owl/fonts/Griffy/Griffy-Regular.ttf') format('ttf'),
+      url('/../scout_owl/fonts/Griffy/Griffy-Regular.ttf') format('ttf');
       font-weight: normal;
       font-style: normal;
       }
       @font-face {
       font-family: 'Comfortaa';
-      src: url('/../stat_owl/fonts/Comfortaa/Comfortaa-Regular.ttf') format('ttf'),
-      url('/../stat_owl/fonts/Comfortaa/Comfortaa-Regular.ttf') format('ttf');
+      src: url('/../scout_owl/fonts/Comfortaa/Comfortaa-Regular.ttf') format('ttf'),
+      url('/../scout_owl/fonts/Comfortaa/Comfortaa-Regular.ttf') format('ttf');
       font-weight: normal;
       font-style: normal;
       }
